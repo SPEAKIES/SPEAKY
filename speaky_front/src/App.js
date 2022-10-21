39b7d5ce4b3bb024410b11c83c1c3e86ef91446d
MO <48742497+msh9900@users.noreply.github.com>
2022-10-21 15:33:31 +0900

    초기세팅

--- speaky_front/src/App.js
+++ speaky_front/src/App.js
@@ -1,10 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Button from "@mui/material/Button";
 import Test from "./components/Test";
+
 function App() {
   return (
     <div className="App">
       <Test />
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
