6c8c65529a12288b04489f63bfbcc9bbaddde85e
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-10-21 16:06:58 +0900

    초기세팅

--- speaky_back/routes/index.js
+++ speaky_back/routes/index.js
@@ -8,11 +8,5 @@ router.get('/', (req, res) => {
   res.render('index', { popup: req.cookies.popup });
 });
 
-router.post('/cookie', (req, res) => {
-  res.cookie('popup', 'hide', {
-    expires: new Date(Date.now() + 1000 * 60 * 60 * 24),
-    httpOnly: true,
-  });
-  res.send('쿠키 생성 성공');
-});
+
 module.exports = router;
