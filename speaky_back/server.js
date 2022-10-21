8549ed59feeac62e4ffa0026855847e0f6b32d53
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-10-21 16:05:58 +0900

    prettier&eslint 추가

--- speaky_back/server.js
+++ speaky_back/server.js
@@ -5,11 +5,19 @@ const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const server = express();
 const passport = require("passport");
+LocalStrategy = require('passport-local').Strategy;
 const PORT = 4000;
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(cors());
+
+server.set("view engine", "ejs");
+server.use("/public", express.static("public"));
+
+// PASSPORT 미들웨어
+server.use(passport.initialize());
+server.use(passport.session());
 
 server.listen(PORT, (req, res) => {
   console.log(`${PORT}로 연결 완료`);
