abbb4da0351b1254a407f69dedc7f69ba9df574f
bogus <qazxwd44@hanmail.net>
2022-10-21 15:26:48 +0900

    back 기본 구성

---
+++ speaky_back/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const monogClient = require("mongoose");
+const cookieParser = require("cookie-parser");
+const multer = require("multer");
+const server = express();
+const PORT = 4000;
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: false }));
+server.use(cors());
+
+server.listen(PORT, (req, res) => {
+  console.log(`${PORT}로 연결 완료`);
+});
