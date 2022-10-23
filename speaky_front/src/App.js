14bf5332fcbb11b9037ad4d505f51d9a3d9a627e
MOS <msh9900@naver.com>
2022-10-23 20:56:35 +0900

    PrimeReact 추가

--- speaky_front/src/App.js
+++ speaky_front/src/App.js
@@ -1,8 +1,15 @@
+// Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
+// PrimeReact
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";                                //icons
+ 
 import Button from "@mui/material/Button";
 import Navbar from "./components/navbar";
 import FreeBoard from "./pages/FreeBoard";
 import CssBaseline from "@mui/material/CssBaseline";
+
 
 function App() {
   return (
