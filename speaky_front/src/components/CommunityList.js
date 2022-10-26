a1904248d665b178e82195f6e360a01286d38c99
MOS <msh9900@naver.com>
2022-10-26 20:51:31 +0900

    자유게시판 미완성

---
+++ speaky_front/src/components/CommunityList.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CommunityList() {
+  return (
+    <div style={{width:'100%',marginTop:'79px'}}>
+        <div style={{width:'100%', height:'300px', backgroundColor:'skyblue',margin:'30px'}}>여기서 구현</div>
+    </div>
+    
+  )
+}
--- speaky_front/src/pages/Community.js
+++ speaky_front/src/pages/Community.js
@@ -1,10 +1,76 @@
 import React from 'react'
 import Header from '../components/Header'
+import Drawer from '@mui/material/Drawer';
+import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import ListSubheader  from '@mui/material/ListSubheader';
+import FollowList from '../components/FollowList';
+import CheckboxList from '../components/CheckboxList';
+import CommunityList from '../components/CommunityList';
+const drawerWidth = '15%';
 export default function Community() {
+  const FollowListData= [
+    {userName:'강한솔',
+    userImage:'경로',},
+    {userName:'모승환',
+    userImage:'경로',},
+    {userName:'이민정',
+    userImage:'경로',},
+    {userName:'천해성',
+    userImage:'경로',},
+
+  ]
   return (
     <>
     <Header/>
-    <div style={{height:500, marginTop:'79px'}}>Community</div>
+    <Box sx={{ display: 'flex' }}>
+    <Drawer
+        sx={{
+          display:{xs:'none',sm:'none',md:'block'},
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            marginTop:9.6,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="left"
+      >
+        
+        <Divider />
+        <ListSubheader component="div" id="nested-list-subheader">
+          날짜
+        </ListSubheader>
+        <CheckboxList/>
+        <Divider />
+        
+      </Drawer>
+      <CommunityList/>
+    <Drawer
+        sx={{
+          display:{xs:'none',sm:'none',md:'block'},
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            marginTop:9.6,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="permanent"
+        anchor="right"
+      >
+       
+        <Divider />
+        <ListSubheader component="div" id="nested-list-subheader">
+          친구창
+        </ListSubheader>
+        <FollowList data={FollowListData}/>
+        <Divider />
+      </Drawer>
+    </Box>
     </>
   )
 }
