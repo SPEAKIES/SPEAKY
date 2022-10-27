058b3a21b56d07a6ea192cd3608abbbafbaf694f
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-10-27 16:31:01 +0900

    dotenv 초기세팅

--- speaky_back/server.js
+++ speaky_back/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const monogClient = require("mongoose");
 const cookieParser = require("cookie-parser");
 const multer = require("multer");
 const server = express();
@@ -8,16 +7,30 @@ const passport = require("passport");
 LocalStrategy = require('passport-local').Strategy;
 const PORT = 4000;
 
+server.set("view engine", "ejs");
+server.use("/public", express.static("public"));
+server.set('views', 'views'); // 공식화
+server.use('/uploads', express.static('uploads'));
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.use(cors());
+server.use(cookieParser('speaky'));
 
-server.set("view engine", "ejs");
-server.use("/public", express.static("public"));
+// dotenv
+require('dotenv').config();
 
-// PASSPORT 미들웨어
-server.use(passport.initialize());
-server.use(passport.session());
+// ROUTER
+const router = require('./routes/index');
+
+server.use('/', router);
+
+// ERROR 처리
+server.use((err, req, res, next) => {
+  console.log(err.stack);
+  res.status(err.statusCode || 500);
+  res.send(err.message);
+});
 
 server.listen(PORT, (req, res) => {
   console.log(`${PORT}로 연결 완료`);
